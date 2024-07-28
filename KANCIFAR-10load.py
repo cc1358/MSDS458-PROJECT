5067a674f14d7ac4aa839a0f02f4db3ed68b711d
cc1358 <32724613+cc1358@users.noreply.github.com>
2024-07-28 14:31:06 -0500

    Update and rename KAN.py to KANCIFAR-10load.py

--- KANCIFAR-10load.py
+++ KANCIFAR-10load.py
@@ -13,3 +13,5 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+
+
